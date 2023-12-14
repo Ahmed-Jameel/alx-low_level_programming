12409754cda6c473bc0fc49df7ca28dd14babb13
Ahmed-Jameel <ahmedalaa.jameel20@gmail.com>
2023-12-14 19:55:55 +0200

    Project 0x00-hello_world, commit tasks 4, 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point of a C program
+ *
+ * Description: This program prints a string literal
+ *
+ * Return: Always (0), success
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point fo C program
+ *
+ * Description: This program prints string literal using printf()
+ *
+ * Return: Always (0), success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

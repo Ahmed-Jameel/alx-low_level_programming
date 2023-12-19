5e34a56fb877906060916977c42db4efaf4f4238
Ahmed-Jameel <ahmedalaa.jameel20@gmail.com>
2023-12-19 17:57:13 +0200

    Commit task 9

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,8 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
+void print_num(int);
+int get_digits_number(int);
+void print_separator(int);
 #endif

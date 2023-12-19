0da386497023afcd761046b47df0d6e218815638
Ahmed-Jameel <ahmedalaa.jameel20@gmail.com>
2023-12-19 19:45:45 +0200

    Project 0x02-functions_nested_loops, Refactoring task 9, 3rd time

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,97 +1,29 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * times_table - It prints 9 times table
+ * times_table - prints 9 time table
  */
 void times_table(void)
 {
-	int i, j, result;
+	int i, j, res;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
 		{
-			int digits = 0;
-
-			result = i * j;
-			digits = get_digits_number(result);
-
-			if (result == 0)
-				_putchar('0');
-			else
-				print_num(result);
-
 			_putchar(',');
+			_putchar(' ');
 
-			if (j != 9)
-			{
-				if (digits == 1)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if (digits == 2)
-					_putchar(' ');
-			}
+			res = i * j;
+
+			if (res <= 9)
+				_putchar(' ');
+			else
+				_putchar((res / 10) + '0');
+
+			_putchar((res % 10) + '0');
 		}
 		_putchar('\n');
-	}
-}
-
-/**
- * print_num - It prints numbers using _putchar
- * @n: a given number to be printed
- */
-void print_num(int n)
-{
-	if (n == 0)
-		return;
-
-	print_num(n / 10);
-	_putchar(n % 10 + '0');
-}
-
-/**
- * get_digits_number - it gets number of digits for a number
- * @n: A given integer
- *
- * Description: It gets the number of digits for a number
- *
- * Return: The number of digits
- */
-int get_digits_number(int n)
-{
-	int cnt = 0;
-
-	if (n == 0)
-		return (1);
-
-	while (n > 0)
-	{
-		cnt++;
-		n /= 10;
-	}
-
-	return (cnt);
-}
-
-/**
- * print_separator -  It prints seperators
- * @digits: The number of digits
- *
- * Description: It prints separators
- */
-void print_separator(int digits)
-{
-	_putchar(',');
-
-	/*printf("DEBUG DIGTS are %d\n", digits);*/
-
-	if (digits > 1)
-		_putchar(' ');
-	else
-	{
-		_putchar(' ');
-		_putchar(' ');
 	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,9 +10,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-void print_num(int);
-int get_digits_number(int);
-void print_separator(int);
 int add(int, int);
 void print_to_98(int n);
 #endif
